1f8133a68d666b8304cb3983fd677e4859d40fcf
Vektor <whiteman.chr@hotmail.com>
2023-03-18 23:34:51 +0100

    fix helpers

--- src/helpers.h
+++ src/helpers.h
@@ -9,13 +9,13 @@ namespace helpers
 	static constexpr int FRAME_SIZE_SAMPLES = SAMPLE_RATE / 10;
 	static constexpr int FRAME_SIZE_BYTES = FRAME_SIZE_SAMPLES * sizeof(int16_t);
 
-	static float GetSignalMultiplierForVolume(float volume)
-	{	//https://github.com/almoghamdani/audify/blob/master/src/rt_audio.cpp#L422
+	inline float GetSignalMultiplierForVolume(float volume)
+	{	//https://github.com/almoghamdani/audify/blob/55a46a2cfba796137310b9d1e09b756d6c58aa7e/src/rt_audio.cpp#L533
 		// Explained here: https://stackoverflow.com/a/1165188
 		return (std::pow<float>(10, volume) - 1) / (10 - 1);
 	}
 
-	static void GainPCM(int16_t* data, size_t framesCount, float gain)
+	inline void GainPCM(int16_t* data, size_t framesCount, float gain)
 	{
 		for (int i = 0; i < framesCount; ++i)
 			data[i] *= gain;
