e9aaf7a63c38acfb157c59eaaf80767e077ab8ba
Vektor <whiteman.chr@hotmail.com>
2023-03-18 22:39:03 +0100

    fix linux platform

--- packages/bass.lua
+++ packages/bass.lua
@@ -4,7 +4,7 @@ package("bass")
         add_versions("24", "eadf39828e0938028a93237fb811efa5cdf16d3670e9658890f948e3f93b8d31")
     end
 
-    if is_plat("linux|x86_64") then
+    if is_plat("linux") then
         add_urls("https://www.un4seen.com/files/bass$(version)-linux.zip")
         add_versions("24", "51bea42d324c5242168b053344a65ed4945715c84f92ed8745e20db33d168ef9")
     end
@@ -15,7 +15,7 @@ package("bass")
         os.cp("c/bass.h", package:installdir("include"))
     end)
 
-    on_install("linux|x86_64", function (package)
+    on_install("linux", function (package)
         os.cp("libs/x86_64/libbass.so", package:installdir("bin"))
         os.cp("bass.h", package:installdir("include"))
     end)
--- packages/bassmix.lua
+++ packages/bassmix.lua
@@ -4,7 +4,7 @@ package("bassmix")
         add_versions("24", "004fc3fc20b38da2641523e01b026b570f63525f2ae4ca9566fe291fbd07242c")
     end
 
-    if is_plat("linux|x86_64") then
+    if is_plat("linux") then
         add_urls("https://www.un4seen.com/files/bassmix$(version)-linux.zip")
         add_versions("24", "a31c4186c8de2ef528760d9238c0c7e30532e676b79f12d3a98901fa25db3a00")
     end
@@ -15,7 +15,7 @@ package("bassmix")
         os.cp("c/bassmix.h", package:installdir("include"))
     end)
 
-    on_install("linux|x86_64", function (package)
+    on_install("linux", function (package)
         os.cp("libs/x86_64/libbassmix.so", package:installdir("bin"))
         os.cp("bassmix.h", package:installdir("include"))
     end)
